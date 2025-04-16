430e521ff1d9321e6d86075999faa2ebd455a5d5
cbonnalie <christianbonnalie@gmail.com>
2025-04-16 12:58:41 -0700

    Initial commit

---
+++ src/database.ts
@@ -0,0 +1,57 @@
+﻿import sqlite3 from "sqlite3";
+import { Database, open } from "sqlite";
+import path from "path";
+
+interface Event {
+  event_type: string;
+  description: string;
+  percent_3months: number;
+  percent_6months: number;
+  percent_1year: number;
+  percent_5years: number;
+}
+
+// absolute path to db
+const dbPath = path.resolve(__dirname, "..", "databases", "events.db");
+
+// define a type for the db connection
+let db: Database | null = null;
+
+// init db connection
+export async function initializeDatabase(): Promise<void> {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+  } catch (err) {
+    console.error("Error initializing database:", err);
+    throw err;
+  }
+}
+
+export function getDatabase(): Database {
+  if (!db) {
+    throw new Error("Database does not exist. Call initializeDatabase() first");
+  }
+  return db;
+}
+
+export async function getFiveEvents(): Promise<Event[] | undefined> {
+  return db?.all<Event[]>(
+    `SELECT *
+         FROM events
+         ORDER BY RANDOM() LIMIT 5
+        `,
+  );
+}
+
+export async function getXEvents(x: number): Promise<Event[] | undefined> {
+  return db?.all<Event[]>(
+    `SELECT *
+         FROM events
+         ORDER BY RANDOM() LIMIT ?
+        `,
+    [x],
+  );
+}
---
+++ src/server.ts
@@ -0,0 +1,77 @@
+﻿import express from "express";
+import cors from "cors";
+import {
+  initializeDatabase,
+  getDatabase,
+  getFiveEvents,
+  getXEvents,
+} from "./database";
+
+const app = express();
+// allows frontend requests
+app.use(cors());
+// allows req.body
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+  try {
+    await initializeDatabase();
+  } catch (err) {
+    console.log("Error fetching database", err);
+  }
+}
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+
+// test API calls
+app.get("/api", async (_, res) => {
+  console.log("/api route hit");
+  res.json({ message: "API is working!" });
+});
+
+app.get("/db", async () => {
+  try {
+    const db = getDatabase();
+    console.log(db);
+    console.log("getDB succeeded");
+  } catch (err) {
+    console.log("Error fetching database", err);
+  }
+});
+
+app.get("/api/fiveEvents", async (req, res) => {
+  try {
+    const events = await getFiveEvents();
+    if (events) {
+      console.log("Events from database:", events);
+      res.header("Content-Type", "application/json");
+      res.json(events);
+    } else {
+      res.status(500).json({ error: "Failed to fetch events" });
+    }
+  } catch (err) {
+    console.error("Error fetching five events:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/xEvents", async (req, res) => {
+  const x = parseInt(req.query.x as string);
+  try {
+    const events = await getXEvents(x);
+    if (events) {
+      console.log("Events from database:", events);
+      res.header("Content-Type", "application/json");
+      res.json(events);
+    } else {
+      res.status(500).json({ error: "Failed to fetch events" });
+    }
+  } catch (err) {
+    console.error("Error fetching events:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+startServer();
