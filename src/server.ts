6b07a0aa63faecc002d1dced460fa0c4ea14b717
christian <christianbonnalie@gmail.com>
2025-04-16 14:32:59 -0700

    comment out db.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 ﻿import express from "express";
 import cors from "cors";
-import {
-  initializeDatabase,
-  getDatabase,
-  getFiveEvents,
-  getXEvents,
-} from "./database";
+// import {
+//   initializeDatabase,
+//   getDatabase,
+//   getFiveEvents,
+//   getXEvents,
+// } from "./database";
 
 const app = express();
 // allows frontend requests
@@ -15,15 +15,13 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-async function startServer() {
-  try {
-    await initializeDatabase();
-  } catch (err) {
-    console.log("Error fetching database", err);
-  }
-}
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+// async function startServer() {
+//   try {
+//     await initializeDatabase();
+//   } catch (err) {
+//     console.log("Error fetching database", err);
+//   }
+// }
 
 // test API calls
 app.get("/api", async (_, res) => {
@@ -31,47 +29,47 @@ app.get("/api", async (_, res) => {
   res.json({ message: "API is working!" });
 });
 
-app.get("/db", async () => {
-  try {
-    const db = getDatabase();
-    console.log(db);
-    console.log("getDB succeeded");
-  } catch (err) {
-    console.log("Error fetching database", err);
-  }
-});
+// app.get("/db", async () => {
+//   try {
+//     const db = getDatabase();
+//     console.log(db);
+//     console.log("getDB succeeded");
+//   } catch (err) {
+//     console.log("Error fetching database", err);
+//   }
+// });
+//
+// app.get("/api/fiveEvents", async (req, res) => {
+//   try {
+//     const events = await getFiveEvents();
+//     if (events) {
+//       console.log("Events from database:", events);
+//       res.header("Content-Type", "application/json");
+//       res.json(events);
+//     } else {
+//       res.status(500).json({ error: "Failed to fetch events" });
+//     }
+//   } catch (err) {
+//     console.error("Error fetching five events:", err);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+//
+// app.get("/api/xEvents", async (req, res) => {
+//   const x = parseInt(req.query.x as string);
+//   try {
+//     const events = await getXEvents(x);
+//     if (events) {
+//       console.log("Events from database:", events);
+//       res.header("Content-Type", "application/json");
+//       res.json(events);
+//     } else {
+//       res.status(500).json({ error: "Failed to fetch events" });
+//     }
+//   } catch (err) {
+//     console.error("Error fetching events:", err);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
-app.get("/api/fiveEvents", async (req, res) => {
-  try {
-    const events = await getFiveEvents();
-    if (events) {
-      console.log("Events from database:", events);
-      res.header("Content-Type", "application/json");
-      res.json(events);
-    } else {
-      res.status(500).json({ error: "Failed to fetch events" });
-    }
-  } catch (err) {
-    console.error("Error fetching five events:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-app.get("/api/xEvents", async (req, res) => {
-  const x = parseInt(req.query.x as string);
-  try {
-    const events = await getXEvents(x);
-    if (events) {
-      console.log("Events from database:", events);
-      res.header("Content-Type", "application/json");
-      res.json(events);
-    } else {
-      res.status(500).json({ error: "Failed to fetch events" });
-    }
-  } catch (err) {
-    console.error("Error fetching events:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-startServer();
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
